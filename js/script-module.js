a6758f85945a5f922e2f31fda8c0080f020cb9b7
Hamza Saddouki <hsaddouki@gmail.com>
2017-04-01 20:19:11 +0200

    Update script-module.js

--- js/script-module.js
+++ js/script-module.js
@@ -3,10 +3,43 @@ $(function() {
     var btn = $(this);
     var action = btn.data('action');
     var device = btn.data('device');
+    var parameters = [];
+    var parameters_name = [];
 
-    $.get('http://tarla.hackathongi.cat/devices/' + device + "/cmds/" + action, function( data ) {
-      console.log( data );
-      alert( "Load was performed." );
+    for (var i = 0; i < name_devices.length; i++) {
+      if (name_devices[i] === device) {
+        console.log(device);
+        for (var j = 0; j < param[i].length; j++) {
+          parameters.push( prompt (param[i][j]) );
+          parameters_name.push( param_name[i][j] );
+        }
+        break;
+      }
+    }
+    var string_parameters = "";
+    if (!(parameters[0] == "" )) {
+      string_parameters = "?";
+      for (var i = 0; i < parameters.length; i++) {
+        if (i===0 && parameters.length==1) {
+            string_parameters += parameters_name[i] + "=" + parameters[i];
+        }else{
+          if (!(parameters[i] === "")) {
+            if (i === parameters.length-1){
+              string_parameters += parameters_name[i] + "=" + parameters[i];
+            }else{
+              string_parameters += parameters_name[i] + "=" + parameters[i] + "&";
+            }
+          }else{
+            if (string_parameters.endsWith("&")) {
+              string_parameters = string_parameters.substring(0, string_parameters.length - 1);
+            }
+            break;
+          }
+        }
+      }
+    }
+    $.get('http://tarla.hackathongi.cat/devices/' + device + "/cmds/" + action + string_parameters, function( data ) {
+      alert(data);
     });
   });
 })
